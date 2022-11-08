5c0798ed7b07af1dba1458fdff057eb8124be8ba
ViktorRasev <103742526+ViktorRasev@users.noreply.github.com>
2022-11-08 12:26:38 +0100

    slider created

--- src/routes/Drivers.js
+++ src/routes/Drivers.js
@@ -1,4 +1,6 @@
 import DriverCard from "../components/DriverCard"
+import Slider from '@mui/material/Slider';
+import { Container } from "react-bootstrap";
 
 
 // todo Slider or select input for seassos (years)
@@ -6,7 +8,19 @@ import DriverCard from "../components/DriverCard"
 export default function Drivers() { 
     return (
         <>
+        <Container>
+       <Slider
+  aria-label="Temperature"
+//   defaultValue={30}
+//   getAriaValueText={valuetext}
+  valueLabelDisplay="auto"
+//   step={10}
+  marks
+  min={1950}
+  max={2022}
+/>
         <DriverCard />
+        </Container>
         </>
     )
 }
