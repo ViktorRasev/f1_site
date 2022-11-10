4825ed4840f827f6d9dc6966b4bfb653ba54b8b6
ViktorRasev <103742526+ViktorRasev@users.noreply.github.com>
2022-11-10 16:23:26 +0100

    Country flags added for drivers

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,11 +5,7 @@
 export default function Home() { 
     return(
         <>
-        <h1>NEXT RACE</h1>
-        <h1>TODO HOME</h1>
-        <h1>TODO HOME</h1>
-        <h1>TODO HOME</h1>
-        <h1>TODO HOME</h1>
+<h1>hello</h1>
         </>
     )
 }
