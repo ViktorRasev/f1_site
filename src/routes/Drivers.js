d5e2469d3d58765bd0ca4ca6797ec49cfedd49cf
ViktorRasev <103742526+ViktorRasev@users.noreply.github.com>
2022-11-16 14:03:09 +0100

    race schedule in home.js added

--- src/routes/Drivers.js
+++ src/routes/Drivers.js
@@ -13,13 +13,13 @@ export default function Drivers() {
 
   // This takes care of mouseUp event even if mouse cursor is no longer hovering over element (Slider)
   const handleMouseUp = useCallback((e) => {
-    document.addEventListener(
-      "mouseup",
-      () => {
-        setInputYears(e.target.textContent);
-      },
-      { once: true }
-    );
+      document.addEventListener(
+        "mouseup",
+        () => {
+          setInputYears(e.target.textContent);
+        },
+        { once: true }
+      );
   }, []);
 
 const handleOnTouchEnd = (e) => { 
@@ -33,7 +33,7 @@ const handleOnTouchEnd = (e) => {
             <Card.Title>Select season</Card.Title>
          
           <Slider
-            className={styles.slider}              
+            // className={styles.slider}              
             onMouseDown={handleMouseUp}
             onTouchEnd={handleOnTouchEnd}
             defaultValue={currentYear}
